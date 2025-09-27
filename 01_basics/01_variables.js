ebb5ff9792855944d3e6ec47f448aa69cddb20d3
RookieStruggler <itzritik.uk2@gmail.com>
2025-09-27 19:18:08 +0000

    data type done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountemail ="ritik@gmail.com"
+var accountpassword = "12345"
+accountcity = "rajasthan"
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
+accountemail = "ram@gmail.com"
+accountpassword = "changed"
+accountcity = "jaipur changed"
+
+console.table([accountemail, accountpassword , accountcity])
---
+++ 01_basics/01datatypes.js
@@ -0,0 +1,7 @@
+console.log(1+1)
+
+console.log("swami")
+
+let name = "ram"
+let age = 18
+let islogin = false
