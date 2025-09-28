4821f86e5d88e04809f1418fd6369ef13bfe600a
RookieStruggler <itzritik.uk2@gmail.com>
2025-09-28 18:52:04 +0000

    DONE

--- 01_basics/01datatypes.js
+++ 01_basics/01datatypes.js
@@ -1,7 +1,31 @@
-console.log(1+1)
+// console.log(1+1)
 
-console.log("swami")
+// // console.log("swami")
 
-let name = "ram"
-let age = 18
-let islogin = false
+// let name = "ram"
+// let age = 18
+// let islogin = false
+
+// age = "18"
+
+// console.log(typeof age);
+// console.log(typeof (age));
+
+// let valueinNumber = Number(age)
+// console.log(typeof valueinNumber)
+
+
+
+
+let x = 3;
+const y = x++;
+
+console.log(x,y)
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+
+console.log(x2,y2);
+
+// x2 is 4n; y2 is 3n
